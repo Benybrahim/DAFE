7ad2fa879937d289a2f213adf6ff6ae3e5d66dca
Khalil Brahim <benybrahim@Khalils-MacBook-Pro.local>
2020-02-10 12:55:44 +0900

    fix: outputs should be float not int

--- DAFE.py
+++ DAFE.py
@@ -2,9 +2,6 @@
 Implementation of DAFE Network paper:
 http://openaccess.thecvf.com/content_ICCVW_2019/papers/CVFAD/Chen_Improving_Fashion_Landmark_Detection_by_Dual_Attention_Feature_Enhancement_ICCVW_2019_paper.pdf
 """
-import os
-import sys
-
 from tensorflow.keras.applications import ResNet50V2
 import tensorflow as tf
 
@@ -38,6 +35,10 @@ def DAFE(input_shape, n_heatmaps):
     x = layers.Conv2D(n_heatmaps, (1, 1), padding='same',
                       name='heatmaps_predictor')(x)
 
+    # estimate keypoints
+    x = layers.Lambda(lambda x: estimate_keypoints(x, input_shape),
+                      name='kp_estimator')(x)
+
     return tf.keras.models.Model(inputs=resnet.input, outputs=x, name='dafe')
 
 
@@ -64,28 +65,52 @@ class _SauBlock(layers.Layer):
 
 
 class _CasBlock(tf.keras.Model):
-  """Channel wise attentive selection block
-  cam: channel-wise attention matrix
-  """
-  def __init__(self, filters, name='', **kwargs):
-      super(_CasBlock, self).__init__(name=name, **kwargs)
-      self.global_pooling = layers.GlobalAveragePooling2D()
-      self.conv1 = layers.Conv2D(filters, (1, 1), name=name+'_conv1',
-                                 padding='same')
-      self.conv2 = layers.Conv2D(filters, (1, 1), name=name+'_conv2',
-                                 padding='same')
+    """Channel wise attentive selection block
+    cam: channel-wise attention matrix
+    """
+    def __init__(self, filters, name='', **kwargs):
+        super(_CasBlock, self).__init__(name=name, **kwargs)
+        self.global_pooling = layers.GlobalAveragePooling2D()
+        self.conv1 = layers.Conv2D(filters, (1, 1), name=name+'_conv1',
+                                   padding='same')
+        self.conv2 = layers.Conv2D(filters, (1, 1), name=name+'_conv2',
+                                   padding='same')
 
-  def call(self, inputs):
-      x = inputs
-      x_global = self.global_pooling(x)
-      x_global = tf.reshape(x_global, shape=(-1, 1, 1, x_global.shape[1]))
-      cam = self.conv1(x_global)
-      cam = tf.nn.relu(cam)
-      cam = self.conv2(cam)
-      return x * cam
+    def call(self, inputs):
+        x = inputs
+        x_global = self.global_pooling(x)
+        x_global = tf.reshape(x_global, shape=(-1, 1, 1, x_global.shape[1]))
+        cam = self.conv1(x_global)
+        cam = tf.nn.relu(cam)
+        cam = self.conv2(cam)
+        return x * cam
+
+
+def estimate_keypoints(heatmaps, input_shape):
+    height, width, _ = input_shape
+    _, heatmap_height, heatmap_width, number_kp = heatmaps.shape
+
+    heatmaps = tf.reshape(heatmaps,
+                          (-1, heatmap_height*heatmap_width , number_kp))
+    idx_max = tf.argmax(heatmaps, axis=1)
+
+    x = idx_max // width
+    y = idx_max % height
+
+    # re-scale keypoints to same input size
+    x_scaled = x * (width // heatmap_width)
+    y_scaled = y * (height // heatmap_height)
+
+    # [[x1, x2, x3, ...], [y1, y2, y3, ...]]
+    keypoints = tf.stack((x_scaled, y_scaled), axis=2)
+
+    # [x1, y1, x2, y2, ...]
+    keypoints = tf.reshape(keypoints, (-1, 2 * number_kp))
+
+    return tf.cast(keypoints, tf.float16)
+
 
 if __name__=='__main__':
-    model = DAFE((320, 256, 3), n_heatmaps=25)
+    model = DAFE((320, 320, 3), n_heatmaps=25)
 
     print(model.summary())
-
